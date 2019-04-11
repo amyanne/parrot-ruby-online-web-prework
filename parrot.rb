6331a17a2201f27971d4a00fa9d0691f0ea9cc1b
Amy Westcott <amy.westcott@yahoo.com>
2019-04-11 20:40:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,4 @@
-def parrot (phrase = "...")# Create method `parrot` that outputs a given phrase and
+def parrot (phrase = "Squawk!")# Create method `parrot` that outputs a given phrase and
   puts phrase# returns the phrase
   phrase
 end
